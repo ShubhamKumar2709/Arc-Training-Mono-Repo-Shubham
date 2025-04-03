0cd882d3d59b66bc36ae7bd7b2b568efd6fe1cf4
shubham <shubham.kumar@sourcefuse.com>
2025-04-03 17:34:23 +0530

    arc rds module

--- Terraform/rds-arc/output.tf
+++ Terraform/rds-arc/output.tf
@@ -3,47 +3,47 @@ output "id" {
   description = "Instance or Cluster ID"
 }
 
-# output "identifier" {
-#   value       = module.rds.identifier
-#   description = "Instance or Cluster Identifier"
-# }
+output "identifier" {
+  value       = module.aurora.identifier
+  description = "Instance or Cluster Identifier"
+}
 
-# output "arn" {
-#   value       = module.rds.arn
-#   description = "Instance or Cluster ARN"
-# }
+output "arn" {
+  value       = module.aurora.arn
+  description = "Instance or Cluster ARN"
+}
 
-# output "username" {
-#   value       = module.rds.username
-#   description = "Username for the Database"
-# }
+output "username" {
+  value       = module.aurora.username
+  description = "Username for the Database"
+}
 
-# output "database" {
-#   value       = module.rds.database
-#   description = "Database name"
-# }
+output "database" {
+  value       = module.aurora.database
+  description = "Database name"
+}
 
-# output "port" {
-#   value       = module.rds.port
-#   description = "Database server port"
-# }
+output "port" {
+  value       = module.aurora.port
+  description = "Database server port"
+}
 
-# output "endpoint" {
-#   value       = module.rds.endpoint
-#   description = "Instance or Cluster Endpoint"
-# }
+output "endpoint" {
+  value       = module.aurora.endpoint
+  description = "Instance or Cluster Endpoint"
+}
 
-# output "kms_key_id" {
-#   value       = module.rds.kms_key_id
-#   description = "Instance or Cluster KMS Key ID"
-# }
+output "kms_key_id" {
+  value       = module.aurora.kms_key_id
+  description = "Instance or Cluster KMS Key ID"
+}
 
-# output "performance_insights_kms_key_id" {
-#   value       = module.rds.performance_insights_kms_key_id
-#   description = "Instance or Cluster Performance Insights KMS Key ID"
-# }
+output "performance_insights_kms_key_id" {
+  value       = module.aurora.performance_insights_kms_key_id
+  description = "Instance or Cluster Performance Insights KMS Key ID"
+}
 
-# output "monitoring_role_arn" {
-#   value       = module.rds.monitoring_role_arn
-#   description = "Instance or Cluster Monitoring Role ARN"
-# }
+output "monitoring_role_arn" {
+  value       = module.aurora.monitoring_role_arn
+  description = "Instance or Cluster Monitoring Role ARN"
+}
